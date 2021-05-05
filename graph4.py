401bf9bfaa61c6702179d1aef7779d5b437ede69
rahul1237 <rahulmahajan98645@gmail.com>
2021-05-06 04:21:32 +0530

    added

---
+++ graph4.py
@@ -0,0 +1,31 @@
+# detection of cycle in the undirected graph using breadth first search (bfs)
+
+n,e=map(int,input().split())
+
+adj=[]
+
+for _ in range(n+1):
+    adj.append([])
+
+for _ in range(e):
+    u,v=map(int,input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+print(adj)
+
+
+# input for number of nodes and edges of the graph
+# 11 10
+
+# input of u and v
+# 1 2
+# 2 4
+# 3 5
+# 5 6
+# 6 7
+# 7 8
+# 5 10
+# 10 9 
+# 8 9
+# 8 11
