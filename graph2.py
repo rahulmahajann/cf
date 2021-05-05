94bd9e930669ca04b0901165a9fbc454e5310c98
rahul1237 <rahulmahajan98645@gmail.com>
2021-05-06 03:36:36 +0530

    added

--- graph2.py
+++ graph2.py
@@ -1,15 +1,15 @@
 # bfs  --> traversing the adjacent nodes of graph at first!
 
-def bfsgraph(n,adj[]):
-    bfs=[]
+# def bfsgraph(n,adj[]):
+#     bfs=[]
 
-    # visited array
-    vis=[]
-    for _ in range(n):
-        vis.append(0)
+#     # visited array
+#     vis=[]
+#     for _ in range(n):
+#         vis.append(0)
 
-    for _ in range(1,n+1):
-        if(vis[_]==0):
+#     for _ in range(1,n+1):
+#         if(vis[_]==0):
             
 
 # numbers of vertex and edge
@@ -19,6 +19,8 @@ n,e=map(int,input().split())
 adj=[]
 for _ in range(n+1):
     adj.append([])
+
+print(adj)
 
 for _ in range(e):
     u,v=map(int,input().split())
@@ -30,9 +32,37 @@ for _ in range(1,n+1):
     print(_,adj[_])
 
 
+vis=[]
+
+for _ in range(n+1):
+    vis.append(0)
 
 
-# 7 6
+
+print(vis)
+
+que=[]
+ans=[]
+
+for _ in range(1,n+1):
+    if(vis[_]==0):
+        que.append(_)
+        vis[_]=1
+        while(len(que)!=0):
+            node=que[0]
+            que.remove(node)
+            ans.append(node)
+
+            for i in adj[node]:
+                if(vis[i]==0):
+                    que.append(i)
+                    vis[i]=1
+
+
+
+print(ans)
+
+# 4 6
 # 1 2
 # 2 3
 # 2 7
