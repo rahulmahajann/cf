6972d98288bacaa561989087f50f80f4df6e6fb1
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-05-14 05:16:18 +0530

    added

---
+++ graph5.py
@@ -0,0 +1,19 @@
+n,e=map(int,input().split())
+
+adj=[]
+vis=[]
+for _ in range(n+1):
+    adj.append([])
+    vis.append(0)
+
+for _ in range(e):
+    u,v=map(int,input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+que=[]
+
+def dfs(node,vis,adj,que):
+    que.append([node,prev])
+    vis[node]=1
+    for _ in adj[node]:
