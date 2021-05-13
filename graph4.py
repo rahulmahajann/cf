797078c352b0856157d6f36cf38da4d82df72461
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-05-14 04:18:45 +0530

    added

--- graph4.py
+++ graph4.py
@@ -1,18 +1,3 @@
-# detection of cycle in the undirected graph using breadth first search (bfs)
-
-n,e=map(int,input().split())
-
-adj=[]
-
-for _ in range(n+1):
-    adj.append([])
-
-for _ in range(e):
-    u,v=map(int,input().split())
-    adj[u].append(v)
-    adj[v].append(u)
-
-print(adj)
 
 
 # input for number of nodes and edges of the graph
@@ -31,31 +16,45 @@ print(adj)
 # 8 11
 
 
+n,e=map(int,input().split())
+
+adj=[]
 vis=[]
 
 for _ in range(n+1):
+    adj.append([])
     vis.append(0)
 
-que=[[0,1]]
+for _ in range(e):
+    u,v=map(int,input().split())
+    adj[u].append(v)
+    adj[v].append(u)
 
+# print('adjacency list: -->')
+
+# for _ in range(1,n+1):
+#     print(_,adj[_])
+# print(adj)
+
+que=[]
+print('ans')
 def cycleinbfs(_):
     que.append([_,-1])
     vis[_]=1
     while(len(que)!=0):
-        node = que[0][0]
-        prev = que[0][1]
+        node=que[0][0]
+        prev=que[0][1]
         que.pop(0)
-        for _ in (adj[node]):
-            if(vis[_]==1):
-                if(_ == prev or _ == -1):
-                    return False
-                else:
+        for _ in adj[node]:
+            if(vis[_]==0):
+                que.append([_,node])
+                vis[_]=1
+            else:
+                if(_ != prev):
                     return True
+    return False
 
-for _ in range(1,n+1):
-    if(vis[_]==0):
-        if(cycleinbfs(_)):
-            print(1)
-        else:
-            print(0)
-
+for i in range(1,n+1):
+    if(vis[i]==0):
+        if(cycleinbfs(i)):
+            print(i,1)
