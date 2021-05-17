d02f05ed86c0cce084262a751bcb471ad8da3674
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-05-18 02:45:35 +0530

    added

--- graph6.py
+++ graph6.py
@@ -12,3 +12,24 @@ for _ in range(e):
     adj[v].append(u)
     adj[u].append(v)
 
+que=[]
+
+def bipar(strt,adj,clr):
+    que.append(strt)
+    clr[strt]='a'
+    while(len(que)!=0):
+        node=que[0]
+        que.pop(0)
+
+        for _ in adj[node]:
+            if(clr[_]==0 and clr[node]=='a'):
+                clr[_]='b'
+                que.append(_)
+            elif(clr[_]==0 and clr[node]=='b'):
+                clr[_]='a'
+                que.append(_)
+            elif(clr[_]==clr[node]):
+                return 0
+    return 1
+
+
